7faa4f1d029c57ce058483f426ab7f1089c99ac0
Andre Sencioles <asenci@gmail.com>
2014-01-06 12:12:40 -0200

    Added licensing information and setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='zabbix_lld',
+    version='0.1',
+    scripts=['zabbix_lld.py'],
+    url='https://bitbucket.org/asenci/zabbix_lld',
+    license='ISC License',
+    author='Andre Sencioles Vitorio Oliveira',
+    author_email='andre@bcp.net.br',
+    description='Low level discovery functions for Zabbix'
+)
